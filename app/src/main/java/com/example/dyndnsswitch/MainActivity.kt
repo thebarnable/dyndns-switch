85b0264b87811c32d5e9fad5d4a4a3e520a6397d
Tim Stadtmann <timstadtmann@gmail.com>
2024-10-03 16:18:23 +0200

    implemented basic structure of main screen

--- app/src/main/java/com/example/dyndnsswitch/MainActivity.kt
+++ app/src/main/java/com/example/dyndnsswitch/MainActivity.kt
@@ -4,56 +4,263 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerScope
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dyndnsswitch.ui.theme.DynDNSSwitchTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
+class Domain (var name: String, var domain: String, var domainAlt: String) {
+    init {
+        Log.d("DEBUG", "Domain $name created for: $domain and $domainAlt")
+        Log.d("DEBUG", "Checking if domains available...")
+        // TODO
+    }
+
+
+}
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DynDNSSwitchTheme {
-                var result by remember { mutableIntStateOf(0) }
+                MainScreen()
+            }
+        }
+    }
 
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Button(onClick = { result++ }) {
-                        Text(text = result.toString())
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun MainScreen() {
+        val state = rememberPagerState(pageCount = {2})
+        val aachenDomains = remember { mutableStateListOf<Domain>()}
+        val kamenDomains = remember { mutableStateListOf<Domain>()}
+
+        Column() {
+            Column(modifier = Modifier
+                .fillMaxHeight(0.95f)
+                .fillMaxWidth()
+            ) {
+                HorizontalPager(
+                    modifier = Modifier,
+                    state = state
+                ) { page ->
+                    if (page == 0)
+                        AachenScreen(aachenDomains)
+                    else
+                        KamenScreen(kamenDomains)
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .height(15.dp)
+                    .fillMaxWidth()
+                ,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                repeat(state.pageCount) { it ->
+                    val color = if (state.currentPage == it) Color.DarkGray else Color.LightGray
+                    Box(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                            .size(15.dp)
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun AachenScreen(domains: MutableList<Domain>, modifier: Modifier = Modifier) {
+        MainScreen(domains, stringResource(id = R.string.aachen), modifier)
+    }
+
+    @Composable
+    fun KamenScreen(domains: MutableList<Domain>, modifier: Modifier = Modifier) {
+        MainScreen(domains, stringResource(id = R.string.kamen), modifier)
+    }
+
+    @Composable
+    fun MainScreen(domains: MutableList<Domain>, title: String, modifier: Modifier = Modifier) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier= Modifier
+                    .fillMaxHeight(0.1f)
+                    .fillMaxWidth(),
+                text=title,
+                fontSize=40.sp,
+                textAlign = TextAlign.Center
+            )
+            Column(
+                modifier = Modifier
+                    .height(600.dp)
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                domains.forEachIndexed {
+                    idx, it -> DomainButton(buttonID = idx, buttonString = it.name)
+                }
+                /*var result by remember { mutableStateOf("Fetching...") }
+                val scope = rememberCoroutineScope()
+
+                LaunchedEffect(Unit) {
+                    scope.launch {
+                        result = httpGet("https://www.topfschlagwm.thebarnable.de")
                     }
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.background
-                    ) {
-                        var result by remember { mutableStateOf("Fetching...") }
-                        val scope = rememberCoroutineScope()
+                }
+                Greeting(result)*/
+            }
+            Button(
+                modifier= Modifier
+                    .weight(1f)
+                    .fillMaxWidth(0.9f),
+                onClick = {
+                    domains.add(Domain("Test", "https://test.thebarnable.de", "https://www.test.thebarnable.de"))
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+    }
 
-                        LaunchedEffect(Unit) {
-                            scope.launch {
-                                result = httpGet("https://www.topfschlagwm.thebarnable.de")
-                            }
-                        }
-                        Greeting(result)
+    @OptIn(ExperimentalFoundationApi::class)
+    @Preview
+    @Composable
+    fun PreviewPageIndicator() {
+        val state = rememberPagerState(pageCount = {2})
+        Column() {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+            ) {
+                Text("test")
+            }
+            Row(
+                modifier = Modifier
+                    .height(20.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                repeat(2) { it ->
+                    val color = if (1 == it) Color.DarkGray else Color.LightGray
+                    Box(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                            .size(20.dp)
+                    )
+                }
+            }
+        }
+    }
+    @Composable
+    fun DomainButton(buttonID : Int, buttonString : String) {
+        Button(
+            onClick = {Log.d("DEBUG", "Pressed domain button")},
+            modifier = Modifier.height(80.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth(0.6f)
+                ) {
+                    Text(
+                        text = buttonString,
+                        fontSize = 30.sp
+                    )
+                }
+                Spacer(modifier = Modifier.width(60.dp))
+                Column(horizontalAlignment = Alignment.End) {
+                    Button(
+                        onClick = { Log.d("DEBUG", "Pressed delete button on $buttonID") },
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.White,
+                            containerColor = Color.Red
+                        ),
+                        modifier = Modifier
+                            .size(width = 80.dp, height = 80.dp)
+                            .padding(0.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowRight,
+                            contentDescription = "Delete",
+                            modifier = Modifier.fillMaxSize()
+                        )
                     }
                 }
             }
         }
+    }
+
+    //@Preview(widthDp = 350)
+    @Composable
+    fun DomainButtonPreview() {
+        DomainButton(0, "Preview")
     }
 
     // Note: we need separate thread for networking operations, imposed by API
@@ -93,18 +300,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = name,
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DynDNSSwitchTheme {
-        Greeting("Preview")
-    }
-}
